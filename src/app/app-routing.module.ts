08fc9ebff9e34d4dd6edeee969841e931e9d73b5
Christian Cano <christian.cano@fouriering.com>
2023-12-09 11:20:13 -0500

    pruducts component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./modules/products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
